dd570f0ccff8f45d95391258b5899f1635e5fc97
victoriabrammeby <92913664+victoriabrammeby@users.noreply.github.com>
2021-10-21 13:26:31 +0200

    Testade ändra en kommentar bara
    Yolo

--- PoddProjekt/Form1.Designer.cs
+++ PoddProjekt/Form1.Designer.cs
@@ -9,7 +9,7 @@ namespace PoddProjekt
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// NU ÄNDRAR JAG DEN HÄR TEXTEN FÖR ATT TESTA
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
